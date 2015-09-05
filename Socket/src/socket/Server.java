1ea2b24902db87e94d9a8fe7256ed236693f3dd5
adam aulia rahmadi <adamaulia242@gmail.com>
2015-09-05 19:36:01 +0700

    hoam...

---
+++ Socket/src/socket/Server.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package socket;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashSet;
+
+/**
+ * A multithreaded chat room server.  When a client connects the
+ * server requests a screen name by sending the client the
+ * text "SUBMITNAME", and keeps requesting a name until
+ * a unique one is received.  After a client submits a unique
+ * name, the server acknowledges with "NAMEACCEPTED".  Then
+ * all messages from that client will be broadcast to all other
+ * clients that have submitted a unique screen name.  The
+ * broadcast messages are prefixed with "MESSAGE ".
+ *
+ * Because this is just a teaching example to illustrate a simple
+ * chat server, there are a few features that have been left out.
+ * Two are very useful and belong in production code:
+ *
+ *     1. The protocol should be enhanced so that the client can
+ *        send clean disconnect messages to the server.
+ *
+ *     2. The server should do some logging.
+ */
+public class Server {
+
+    /**
+     * The port that the server listens on.
+     */
+    private static final int PORT = 9001;
+
+    /**
+     * The set of all names of clients in the chat room.  Maintained
+     * so that we can check that new clients are not registering name
+     * already in use.
+     */
+    private static HashSet<String> names = new HashSet<String>();
+
+    /**
+     * The set of all the print writers for all the clients.  This
+     * set is kept so we can easily broadcast messages.
+     */
+    private static HashSet<PrintWriter> writers = new HashSet<PrintWriter>();
+
+    /**
+     * The appplication main method, which just listens on a port and
+     * spawns handler threads.
+     */
+    public static void main(String[] args) throws Exception {
+        System.out.println("Server ON ");
+        ServerSocket listener = new ServerSocket(PORT);
+        try {
+            while (true) {
+                new ServerThread(listener.accept()).start();
+            }
+        } finally {
+            listener.close();
+        }
+    }
+
+    /**
+     * A handler thread class.  Handlers are spawned from the listening
+     * loop and are responsible for a dealing with a single client
+     * and broadcasting its messages.
+     */
+
+}

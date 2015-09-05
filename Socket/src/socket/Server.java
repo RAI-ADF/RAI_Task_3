403b3c4628239580cfdcca593ca86260637d74a4
adam aulia rahmadi <adamaulia242@gmail.com>
2015-09-05 19:43:34 +0700

    hoam 2

--- Socket/src/socket/Server.java
+++ Socket/src/socket/Server.java
@@ -15,48 +15,23 @@ import java.net.Socket;
 import java.util.HashSet;
 
 /**
- * A multithreaded chat room server.  When a client connects the
- * server requests a screen name by sending the client the
- * text "SUBMITNAME", and keeps requesting a name until
- * a unique one is received.  After a client submits a unique
- * name, the server acknowledges with "NAMEACCEPTED".  Then
- * all messages from that client will be broadcast to all other
- * clients that have submitted a unique screen name.  The
- * broadcast messages are prefixed with "MESSAGE ".
  *
- * Because this is just a teaching example to illustrate a simple
- * chat server, there are a few features that have been left out.
- * Two are very useful and belong in production code:
- *
- *     1. The protocol should be enhanced so that the client can
- *        send clean disconnect messages to the server.
- *
- *     2. The server should do some logging.
+ * @author Adam
+ * original copy from 
+ * http://cs.lmu.edu/~ray/notes/javanetexamples/
  */
 public class Server {
 
-    /**
-     * The port that the server listens on.
-     */
+
     private static final int PORT = 9001;
 
-    /**
-     * The set of all names of clients in the chat room.  Maintained
-     * so that we can check that new clients are not registering name
-     * already in use.
-     */
+
     private static HashSet<String> names = new HashSet<String>();
 
-    /**
-     * The set of all the print writers for all the clients.  This
-     * set is kept so we can easily broadcast messages.
-     */
+
     private static HashSet<PrintWriter> writers = new HashSet<PrintWriter>();
 
-    /**
-     * The appplication main method, which just listens on a port and
-     * spawns handler threads.
-     */
+
     public static void main(String[] args) throws Exception {
         System.out.println("Server ON ");
         ServerSocket listener = new ServerSocket(PORT);
@@ -69,10 +44,6 @@ public class Server {
         }
     }
 
-    /**
-     * A handler thread class.  Handlers are spawned from the listening
-     * loop and are responsible for a dealing with a single client
-     * and broadcasting its messages.
-     */
-
+   
+    
 }
--- Socket/src/socket/ServerThread.java
+++ Socket/src/socket/ServerThread.java
@@ -41,15 +41,12 @@ public class ServerThread extends Thread {
         public void run() {
             try {
 
-                // Create character streams for the socket.
+
                 in = new BufferedReader(new InputStreamReader(
                 socket.getInputStream()));
                 out = new PrintWriter(socket.getOutputStream(), true);
 
-                // Request a name from this client.  Keep requesting until
-                // a name is submitted that is not already used.  Note that
-                // checking for the existence of a name and adding the name
-                // must be done while locking the set of names.
+
                 while (true) {
                     out.println("SUBMITNAME");
                     name = in.readLine();
@@ -64,14 +61,11 @@ public class ServerThread extends Thread {
                     }
                 }
 
-                // Now that a successful name has been chosen, add the
-                // socket's print writer to the set of all writers so
-                // this client can receive broadcast messages.
+
                 out.println("NAMEACCEPTED");
                 writers.add(out);
 
-                // Accept messages from this client and broadcast them.
-                // Ignore other clients that cannot be broadcasted to.
+
                 while (true) {
                     String input = in.readLine();
                     if (input == null) {
@@ -84,8 +78,7 @@ public class ServerThread extends Thread {
             } catch (IOException e) {
                 System.out.println(e);
             } finally {
-                // This client is going down!  Remove its name and its print
-                // writer from the sets, and close its socket.
+
                 if (name != null) {
                     names.remove(name);
                 }

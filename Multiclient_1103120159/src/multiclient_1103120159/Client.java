bceb9052519e40f9ddacff5bef2e522657b492f1
Fauzan <mfr.belato@gmail.com>
2015-09-08 06:03:24 +0700

    RAI_Task_3_1103120159
    Referensi: http://cs.lmu.edu/~ray/notes/javanetexamples/

--- Multiclient_1103120159/src/multiclient_1103120159/Client.java
+++ Multiclient_1103120159/src/multiclient_1103120159/Client.java
@@ -22,12 +22,12 @@ import javax.swing.*;
  * @author Fauzan Razandi
  */
 public class Client {
-    BufferedReader inputStream;
-    PrintWriter outputStream;
-    String serverAddress;
-    JFrame frame = new JFrame("1103120159-CHATBOX");
-    JTextField messageInput = new JTextField(40);
-    JTextArea messageArea = new JTextArea(8, 40);
+   static BufferedReader inputStream;
+   static PrintWriter outputStream;
+   static String serverAddress;
+   static JFrame frame = new JFrame("1103120159-CHATBOX");
+   static JTextField messageInput = new JTextField(40);
+   static JTextArea messageArea = new JTextArea(8, 40);
     
     public Client(){
         messageInput.setEditable(false);
@@ -46,50 +46,11 @@ public class Client {
         });
     }
     
-    public String getServerAddress(){
-        return JOptionPane.showInputDialog(
-               frame,
-               "Enter IP Address:",
-               "Welcome to CHATBOX",
-               JOptionPane.QUESTION_MESSAGE);
-    }
-    
-    public String getUsername(){
-        return JOptionPane.showInputDialog(
-            frame,
-            "Input your username:",
-            "Your username is created",
-            JOptionPane.PLAIN_MESSAGE);
-    }
-    
-    public void run() throws IOException{
-        serverAddress = getServerAddress();
-        Socket clientSocket = new Socket("localhost",2222);
-        inputStream = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        outputStream = new PrintWriter(clientSocket.getOutputStream(), true);
-            while(true){
-                String message = inputStream.readLine();
-                if(message.startsWith("USERNAME")){
-                    outputStream.println(getUsername());
-                }else if(message.startsWith("ACCEPTEDUSERNAME")){
-                    messageInput.setEditable(true);
-                }else if(message.startsWith("MESSAGE")){
-                    messageArea.append(message.substring(8) + "\n");
-                }
-            }
- 
-       
-        
-    }
-    
-    
-
     public static void main(String[] args) throws IOException{
         Client client = new Client();
-        
         client.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         client.frame.setVisible(true);
-        client.run(); 
+        ReadInput.run();
         
     }
 }
--- Multiclient_1103120159/src/multiclient_1103120159/ReadInput.java
+++ Multiclient_1103120159/src/multiclient_1103120159/ReadInput.java
@@ -6,35 +6,57 @@
 package multiclient_1103120159;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 /**
  *
  * @author Fauzan Razandi
  */
 public class ReadInput {
-   
-    BufferedReader inputStream;
     
-    public ReadInput(BufferedReader inputStream){
-        this.inputStream = inputStream;
+  
+  
+     public static String getServerAddress(){
+        return JOptionPane.showInputDialog(
+               Client.frame,
+               "Enter IP Address:",
+               "Welcome to CHATBOX",
+               JOptionPane.QUESTION_MESSAGE);
     }
-    public void run(){
-        try{
-            Client client = new Client();
-            System.out.print("Chat Broadcast: ");
-            String inputan = inputStream.readLine();
-            System.out.println("");
-            while((inputan)!= null){
-                client.messageArea.append(inputan.substring(8) + "\n");
+    
+     
+    public static String getUsername(){
+        return JOptionPane.showInputDialog(
+            Client.frame,
+            "Input your username:",
+            "Your username is created",
+            JOptionPane.PLAIN_MESSAGE);
+    }
+    
+    public static void run() throws IOException{
+        Client.serverAddress = getServerAddress();
+        Socket clientSocket = new Socket("localhost",2222);
+        Client.inputStream = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        Client.outputStream = new PrintWriter(clientSocket.getOutputStream(), true);
+            while(true){
+                String message = Client.inputStream.readLine();
+                if(message.startsWith("USERNAME")){
+                    Client.outputStream.println(getUsername());
+                }else if(message.startsWith("ACCEPTEDUSERNAME")){
+                    Client.messageInput.setEditable(true);
+                }else if(message.startsWith("MESSAGE")){
+                    Client.messageArea.append(message.substring(8) + "\n");
+                }
             }
-        }catch(Exception e){
-            System.out.println(e);
-        }
-        
     }
+    
+  
 
 }

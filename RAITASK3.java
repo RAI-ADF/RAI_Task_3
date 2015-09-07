a203c82bd6e1c7ddbb6c641f379f798e68175db7
Aulia Marchita Ramadhani <auliamarchita@gmail.com>
2015-09-07 18:52:00 +0700

    Create RAITASK3.java

---
+++ RAITASK3.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package raitask3;
+
+import javax.swing.*;
+/**
+ *
+ * @author auliamarchita
+ */
+
+public class RAITASK3 {
+
+	
+	public static void main(String [] args){
+		
+		Object[] selectioValues = { "Server","Client"};
+		String initialSection = "Server";
+		
+		Object selection = JOptionPane.showInputDialog(null, "Login as : ", "Multiple Client Chat", JOptionPane.QUESTION_MESSAGE, null, selectioValues, initialSection);
+		if(selection.equals("Server")){
+                   String[] arguments = new String[] {};
+			new Server().main(arguments);
+		}else if(selection.equals("Client")){
+			String IPServer = JOptionPane.showInputDialog("Enter the server name : ");
+                        String[] arguments = new String[] {IPServer};
+			new Client().main(arguments);
+		}
+		
+	}
+
+}

f2a946736a5944332f2e46698ce7ae3f9b3029fb
saputrasuryaa <saputra.surya13@gmail.com>
2015-09-07 20:21:04 +0700

    Task RAI 3 - 1103124304

---
+++ MainServer.java
@@ -0,0 +1,15 @@
+/**
+ *
+ * @author Surya Saputra
+ */
+public class MainServer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        guiServer server = new guiServer();
+        server.setVisible(true);
+    }
+    
+}

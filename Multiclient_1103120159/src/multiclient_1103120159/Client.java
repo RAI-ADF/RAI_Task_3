3020c8c6ea2daa43507abe2dab90bdc0b6915348
Fauzan <mfr.belato@gmail.com>
2015-09-08 04:06:58 +0700

    Perubahan Client.java dan ReadInput.java

--- Multiclient_1103120159/src/multiclient_1103120159/Client.java
+++ Multiclient_1103120159/src/multiclient_1103120159/Client.java
@@ -5,6 +5,9 @@
  */
 package multiclient_1103120159;
 
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -12,38 +15,80 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.*;
 
 /**
  *
  * @author Fauzan Razandi
  */
 public class Client {
-    public static void main(String[] args){
-        try {
-            BufferedReader keyBoard = new BufferedReader(new InputStreamReader(System.in));
-            System.out.print("Input Server IP : ");
-            String ip = keyBoard.readLine();
-            Socket connect = new Socket("localhost",2222);
-            
-            BufferedReader inputStream = new BufferedReader(new InputStreamReader(connect.getInputStream()));
-            PrintWriter outputSream = new PrintWriter (connect.getOutputStream(), true);
+    private BufferedReader inputStream;
+    private PrintWriter outputStream;
+    private String serverAddress;
+    private Socket clientSocket;
+    JFrame frame = new JFrame("1103120159-CHATBOX");
+    JTextField messageInput = new JTextField(50);
+    JTextArea messageArea = new JTextArea(10, 50);
     
-            ReadInput in = new ReadInput(inputStream);
-            in.run();
-            
-            String inputKeyBoard;
-            do{
-                System.out.println(">> ");
-                inputKeyBoard = keyBoard.readLine();
-                outputSream.println(inputKeyBoard);
-                outputSream.flush();
-            }while(!inputKeyBoard.equals("quit"));
-            
-        } catch (IOException e) {
-            System.out.println("Failed to Connect " + e);
-        }
+    public Client(){
+        messageInput.setEditable(false);
+        messageArea.setEditable(false);
+        frame.getContentPane().add(messageInput, "North");
+        frame.getContentPane().add(new JScrollPane(messageArea), "Center");
+        frame.pack();
+        
+      messageInput.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            outputStream.println(messageInput.getText());
+            messageInput.setText("");
+            }
+        });
+    }
+    
+    private String getServerAddress(){
+        return JOptionPane.showInputDialog(
+               frame,
+               "Enter IP Address:",
+               "Welcome to CHATBOX",
+               JOptionPane.QUESTION_MESSAGE);
+    }
+    
+    private String getUsername(){
+        return JOptionPane.showInputDialog(
+            frame,
+            "Input your username:",
+            "Your username is created",
+            JOptionPane.PLAIN_MESSAGE);
+    }
+    
+    private void run() throws IOException{
+        serverAddress = "localhost";
+        clientSocket = new Socket(serverAddress,2222);
+        inputStream = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        outputStream = new PrintWriter(clientSocket.getOutputStream(), true);
+            while(true){
+                String message = inputStream.readLine();
+                if(message.startsWith("SUBMITUSERNAME")){
+                    outputStream.println(getUsername());
+                }else if(message.startsWith("USERNAMEACCEPTED")){
+                    messageInput.setEditable(true);
+                }else if(message.startsWith("MESSAGE")){
+                    ReadInput readInput = new ReadInput();
+                    readInput.run();
+                }
+            }
+       
         
     }
     
     
+
+    public static void main(String[] args) throws IOException{
+        Client client = new Client();
+        client.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        client.frame.setVisible(true);
+        client.run(); 
+    }
 }
--- Multiclient_1103120159/src/multiclient_1103120159/ReadInput.java
+++ Multiclient_1103120159/src/multiclient_1103120159/ReadInput.java
@@ -6,6 +6,10 @@
 package multiclient_1103120159;
 
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import javax.swing.JTextArea;
 
 /**
  *
@@ -13,18 +17,14 @@ import java.io.BufferedReader;
  */
 public class ReadInput {
     private BufferedReader inputStream;
+    private PrintWriter outputStream;
+    private Socket clientSocket;
+    JTextArea messageArea = new JTextArea(10, 50);
     
-    public ReadInput(BufferedReader inputStream){
-        this.inputStream = inputStream;
-    }
-
     public void run(){
         try{
-            String inputan;
-            while((inputan = inputStream.readLine()) !=null){
-                System.out.println(inputan);
-                System.out.println(">> ");
-            }
+            String message = inputStream.readLine();
+            messageArea.append(message.substring(10) + "\n");
         }catch(Exception e){
             System.out.println(e);
         }
